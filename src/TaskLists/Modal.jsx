917f051dce687e1a60a8e11f3d4cbeaf4efaa65b
MUSHFIQUER-RAKIT <mushfiquerrakit@gmail.com>
2024-10-27 17:49:06 +0600

    Unknown

--- src/TaskLists/Modal.jsx
+++ src/TaskLists/Modal.jsx
@@ -24,11 +24,10 @@ export default function Modal() {
     }));
   };
 
-  const handleSubmit = event => {
+  const handleSubmit = (taskDetails, event) => {
     event.preventDefault();
-    const { id, name, description, date } = taskDetails;
 
-    if ((id, name && description && date)) {
+    if (taskDetails.name && taskDetails.description && taskDetails.date) {
       addTask(category, taskDetails, isAdd);
       closeModal();
     } else {
@@ -125,7 +124,7 @@ export default function Modal() {
                   Cancel
                 </button>
                 <button
-                  onClick={() => handleSubmit(event)}
+                  onClick={() => handleSubmit(taskDetails, event)}
                   type="submit"
                   className="rounded-md border border-transparent bg-green-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-800"
                 >
